c274dc2c9a95b6bcbe7890350a35857e9c0d5da9
hexi98 <381326816@qq.com>
2023-05-31 15:44:14 +0800

    网页+生成器-test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from markupsafe import escape
-
+import test as poem_gen
+import json
 name = 'He xi'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -25,3 +26,16 @@ app = Flask(__name__)
 @app.route('/home')
 def index():
     return render_template('index.html',name = name,movies = movies)
+
+@app.route('/test',methods=['POST'])
+def test():
+    result = {"poem":poem_gen.userTest()}
+    print(result)
+    # data = request.get_json()
+    return result
+
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
