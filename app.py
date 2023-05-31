d261574c613e20b9ade56e13249cf41e42ce3037
hexi98 <381326816@qq.com>
2023-05-31 16:29:32 +0800

    词汇添加到html中

--- app.py
+++ app.py
@@ -20,18 +20,18 @@ app = Flask(__name__)
 # @app.route('/user/<name>')
 # def user_page(name):
 #     return f'User: {escape(name)}'
+locationNames = ["钟山","青溪","石头城","三山","新亭","凤凰台","乌衣巷","白鹭洲","玄武湖","冶城","雨花台","方山","朱雀桥","秦淮河","东山","摄山","牛首山","中华门","桃叶渡","长干里","覆舟山","莫愁湖","景阳楼","瓦官寺","光宅寺","新街口","新林浦","清凉寺","燕子矶","劳劳亭","随园","八功德水","中山门","内桥","北门桥","杏花村","谢公墩","半山园","挹江门","麾扇渡","中山路","大行宫","状元境","饮马巷","大王府巷","泥马巷","金銮巷","落星岗","白门"]
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html',name = name,movies = movies)
+    return render_template('index.html',locationNames = locationNames)
 
-@app.route('/test',methods=['POST'])
-def test():
-    result = {"poem":poem_gen.userTest()}
-    print(result)
-    # data = request.get_json()
+@app.route('/test/<LocationName>',methods=['POST'])
+def test(LocationName):
+    result = {"poem":poem_gen.userTest(LocationName)}
+    data = request.get_json()
     return result
 
 
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import torch as t
 from generate import *
 
 
-def userTest():
+def userTest(LocationName):
     print("正在初始化......")
     datas = np.load("tang.npz",allow_pickle=True)
     data = datas['data']
@@ -16,37 +16,44 @@ def userTest():
     if Config.use_gpu:
         model.to(t.device('cuda'))
     print("初始化完成！\n")
-    # while True:
-        
-    print("\n欢迎使用诗词生成器，\n"
-            "输入1 进入首句生成模式\n"
-            "输入2 进入藏头诗生成模式\n")
-    location_list = ["钟山","青溪","石头城","三山","新亭","凤凰台","乌衣巷","白鹭洲","玄武湖","冶城","雨花台","方山","朱雀桥","秦淮河","东山","摄山","牛首山","中华门","桃叶渡","长干里","覆舟山","莫愁湖","景阳楼","瓦官寺","光宅寺","新街口","新林浦","清凉寺","燕子矶","劳劳亭","随园","八功德水","中山门","内桥","北门桥","杏花村","谢公墩","半山园","挹江门","麾扇渡","中山路","大行宫","状元境","饮马巷","大王府巷","泥马巷","金銮巷","落星岗","白门"]
-    
-    mode = int(input())
-    if mode == 1:
+  
         # print("请输入您想要的诗歌首句，可以是五言或七言")
-        print("请选择地点：")
-        for i in range(len(location_list)):
-            print(location_list[i],end=' ')
-            if(i+1)% 10 == 0:
-                print("\n") 
+       
+    start_words = str(LocationName)
+    gen_poetry = ''.join(generate(model, start_words, ix2word, word2ix))
+    print("生成的诗句如下：%s\n" % (gen_poetry))
+    
+    
+    # while True:
+    # print("\n欢迎使用诗词生成器，\n"
+    #         "输入1 进入首句生成模式\n"
+    #         "输入2 进入藏头诗生成模式\n")
+    # location_list = ["钟山","青溪","石头城","三山","新亭","凤凰台","乌衣巷","白鹭洲","玄武湖","冶城","雨花台","方山","朱雀桥","秦淮河","东山","摄山","牛首山","中华门","桃叶渡","长干里","覆舟山","莫愁湖","景阳楼","瓦官寺","光宅寺","新街口","新林浦","清凉寺","燕子矶","劳劳亭","随园","八功德水","中山门","内桥","北门桥","杏花村","谢公墩","半山园","挹江门","麾扇渡","中山路","大行宫","状元境","饮马巷","大王府巷","泥马巷","金銮巷","落星岗","白门"]
+    
+    # mode = int(input())
+    # if mode == 1:
+    #     # print("请输入您想要的诗歌首句，可以是五言或七言")
+    #     print("请选择地点：")
+    #     for i in range(len(location_list)):
+    #         print(location_list[i],end=' ')
+    #         if(i+1)% 10 == 0:
+    #             print("\n") 
         
-        print("\n") 
-        start_words = str(input())
-        gen_poetry = ''.join(generate(model, start_words, ix2word, word2ix))
-        print("生成的诗句如下：%s\n" % (gen_poetry))
-    elif mode == 2:
-        print("请选择诗词地点：")
-        for i in range(len(location_list)):
-            print(location_list[i],end=' ')
-            if(i+1)% 10 == 0:
-                print("\n") 
-        print("\n")
-        start_words = str(input())
-        gen_poetry = ''.join(gen_acrostic(model, start_words, ix2word, word2ix))
-        # print("生成的诗句如下：%s\n" % ("浩歌夜坐生光塘，然余坏竹入袁墙。最爱林泉多往事，喜逢日月共流光。欢讴未暇听雷响，芷壑已惊蛛雁忙。若无一年离世曰，宝莲山中有仙郎。"))
-        print("生成的诗句如下：%s\n" % (gen_poetry))
+    #     print("\n") 
+    #     start_words = str(input())
+    #     gen_poetry = ''.join(generate(model, start_words, ix2word, word2ix))
+    #     print("生成的诗句如下：%s\n" % (gen_poetry))
+    # elif mode == 2:
+    #     print("请选择诗词地点：")
+    #     for i in range(len(location_list)):
+    #         print(location_list[i],end=' ')
+    #         if(i+1)% 10 == 0:
+    #             print("\n") 
+    #     print("\n")
+    #     start_words = str(input())
+    #     gen_poetry = ''.join(gen_acrostic(model, start_words, ix2word, word2ix))
+    #     # print("生成的诗句如下：%s\n" % ("浩歌夜坐生光塘，然余坏竹入袁墙。最爱林泉多往事，喜逢日月共流光。欢讴未暇听雷响，芷壑已惊蛛雁忙。若无一年离世曰，宝莲山中有仙郎。"))
+    #     print("生成的诗句如下：%s\n" % (gen_poetry))
     return gen_poetry
 
 if __name__ == '__main__':
