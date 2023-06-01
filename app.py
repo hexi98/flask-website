3a55b344c2f9bc2827cc5bf6e4542e43f83068ff
hexi98 <381326816@qq.com>
2023-06-01 16:06:14 +0800

    代码整合

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template,request
 from markupsafe import escape
-import test as poem_gen
-import json
+from static.python.main import *
+from static.python.model import *
+from static.python.config import *
+import torch as t
+from static.python.generate import *
+
+
+def userTest(LocationName):
+    print("正在初始化......")
+    datas = np.load("static/python/tang.npz",allow_pickle=True)
+    data = datas['data']
+    ix2word = datas['ix2word'].item()
+    word2ix = datas['word2ix'].item()
+    model = PoetryModel(len(ix2word), Config.embedding_dim, Config.hidden_dim)
+    model.load_state_dict(t.load(Config.model_path, 'cpu'))
+    if Config.use_gpu:
+        model.to(t.device('cuda'))
+    print("初始化完成！\n")
+    # print("请输入您想要的诗歌首句，可以是五言或七言")
+    start_words = str(LocationName)
+    gen_poetry = ''.join(generate(model, start_words, ix2word, word2ix))
+    print("生成的诗句如下：%s\n" % (gen_poetry))
+    
+    return gen_poetry
+
 name = 'He xi'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -17,9 +40,6 @@ movies = [
 ]
 app = Flask(__name__)
 
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return f'User: {escape(name)}'
 locationNames = ["钟山","青溪","石头城","三山","新亭","凤凰台","乌衣巷","白鹭洲","玄武湖","冶城","雨花台","方山","朱雀桥","秦淮河","东山","摄山","牛首山","中华门","桃叶渡","长干里","覆舟山","莫愁湖","景阳楼","瓦官寺","光宅寺","新街口","新林浦","清凉寺","燕子矶","劳劳亭","随园","八功德水","中山门","内桥","北门桥","杏花村","谢公墩","半山园","挹江门","麾扇渡","中山路","大行宫","状元境","饮马巷","大王府巷","泥马巷","金銮巷","落星岗","白门"]
 
 @app.route('/')
@@ -30,7 +50,7 @@ def index():
 
 @app.route('/test/<LocationName>',methods=['POST'])
 def test(LocationName):
-    result = {"poem":poem_gen.userTest(LocationName)}
+    result = {"poem":userTest(LocationName)}
     data = request.get_json()
     return result
 
